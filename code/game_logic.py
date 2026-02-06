d3efb248c80ccdee41520c1536576349034d75da
gennarocarminetozza <gennarocarmine@gmail.com>
2026-02-06 18:31:34 +0100

    feat: added the logic of the game

---
+++ code/game_logic.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+PLAYER_PIECE = 1
+AI_PIECE = -1 
+EMPTY = 0
+
+def create_board():
+    return np.zeros((ROW_COUNT, COLUMN_COUNT))
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def get_valid_locations(board):
+    return [c for c in range(COLUMN_COUNT) if is_valid_location(board, c)]
+
+def winning_move(board, piece):
+    # Controllo Orizzontale
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    # Controllo Verticale
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+    # Diagonale Positiva
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    # Diagonale Negativa
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+    return False
+
+def get_winning_coordinates(board, piece):
+    # Orizzontale
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return [(r, c), (r, c+1), (r, c+2), (r, c+3)]
+    # Verticale
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return [(r, c), (r+1, c), (r+2, c), (r+3, c)]
+    # Diagonale Positiva
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return [(r, c), (r+1, c+1), (r+2, c+2), (r+3, c+3)]
+    # Diagonale Negativa
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return [(r, c), (r-1, c+1), (r-2, c+2), (r-3, c+3)]
+    return []
